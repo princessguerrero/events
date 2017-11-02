73a460670e9e4e39604e36d71bfc21944883d240
princessguerrero <princessguerrero@ac.c4q.nyc>
2017-11-02 10:29:02 -0400

    code modifications

--- Quadtoggle/quadrant-toggle.js
+++ Quadtoggle/quadrant-toggle.js
@@ -1,17 +1,7 @@
-document.addEventListener('click', function(event) {
-    var w = window.innerWidth;
-    var h = window.innerHeight;
-    var topLeft = document.querySelector("#topLeft")
-    
-    var body = document.querySelector('body')
-    if ((w / 2) > event.clientX ) {
-        body.style.backgroundColor = "blue"
-    } else if ((w / 2) < event.clientX) {
-        body.style.backgroundColor = "red"
-    } else if ((h / 2) > event.clientY) {
-        body.style.backgroundColor = "black"
-    } else {
-        body.style.backgroundColor = "white"
-    }
-    
-}) 
+document.addEventListener('click', function (event) {
+    var quad = event.target.style
+
+    quad.backgroundColor === "purple" ? quad.backgroundColor = "white" :
+        quad.backgroundColor = "purple"
+});
+
