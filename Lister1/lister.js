d5d702c7533d8798eca49b9d33dd9919ed316bbb
princessguerrero <princessguerrero@ac.c4q.nyc>
2017-11-03 11:47:51 -0400

    added comments and changed let to var

--- Lister1/lister.js
+++ Lister1/lister.js
@@ -1,7 +1,11 @@
-function newList() {
-    var input =  document.querySelector("#text")
+function addListItem() {
+    var input = document.querySelector("#text")
+    // document.querySelector('#todos li:nth-child(2)')
+    console.log('adding input: ', input.value)
+    window.localStorage.setItem('lastInput', input.value)
+
     if (input.value) {
-        let list = document.querySelector("ul");
+        let list = document.querySelector("#todos");
         let newItem = document.createElement("li");
         newItem.innerText = input.value
         list.appendChild(newItem)
@@ -9,30 +13,39 @@ function newList() {
     }
 }
 
-var listItems = document.querySelector("ul");
-
+var listItems = document.querySelector("#todos");
 listItems.addEventListener("click", function (event) {
     let target = event.target
     if (target.tagName.toUpperCase() == 'LI') {
-        target.style.textDecoration = 
-        target.style.textDecoration !== 'line-through' ? 'line-through' : 'none'
+        target.style.textDecoration = target.style.textDecoration !== 'line-through' ? 'line-through' : 'none'
     }
 })
 
-var submit = document.querySelector("#submit");
+let submit = document.querySelector("#addnew");
 submit.addEventListener("click", function (event) {
     event.preventDefault();
-    newList();
-    // console.log(listItems.childNodes.length);
+    //this is to prevent the page from refreshing when user press Submit button
+    addListItem();
 })
 
-var button = document.querySelector("#removedone");
-button.addEventListener("click", function (event) {
-    var listItems = document.querySelectorAll('li')
-    var list = document.querySelector('ul')
-    for (var i = 0; i < listItems.length; i++){
-        if(listItems[i].style.textDecoration === 'line-through'){
+let btnRemDone = document.querySelector("#removedone");
+btnRemDone.addEventListener("click", function (event) {
+    var listItems = document.querySelectorAll('#todos li')
+    //querySelector works like CSS in selecting (like parent-child)
+    console.log(listItems)
+    var list = document.querySelector('#todos')
+
+    for (var i = 0; i < listItems.length; i++) {
+        if (listItems[i].style.textDecoration === 'line-through') {
             list.removeChild(listItems[i])
         }
     }
 })
+
+// This code is executed When the page loads
+var lastInput = window.localStorage.getItem('lastInput')
+console.log('last Input: ', lastInput)
+
+
+
+
